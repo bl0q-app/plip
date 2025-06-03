908858db688f30d46da175eea1bb18e793940c77
ru-dr <Rudra26_2004@outlook.com>
2025-06-03 13:02:23 +0530

    🧹 [CHORE] : restructure monorepo and update package configurations * reorganize file paths and bump plip package version to 1.1.1

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,6 +1,6 @@
 import type { DefaultTheme } from 'vitepress'
 import { defineConfig } from 'vitepress'
-import { version } from '../../package.json'
+import { version } from '../../packages/npm/package.json'
 
 const GETTING_STARTED: DefaultTheme.NavItemWithLink[] = [
   { text: 'Introduction', link: '/introduction' },
